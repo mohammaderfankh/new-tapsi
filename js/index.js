94a08922238b884ed9abe36afceda1440e73aad5
mohammaderfankh <79629565+mohammaderfankh@users.noreply.github.com>
2022-06-30 13:47:50 +0430

    update navbar section

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// navbar
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
